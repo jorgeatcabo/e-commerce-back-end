7c4f4fe7311c84e043cf193caefc874bb07ab336
El Roble <san_lucas2005@yahoo.com>
2021-06-03 17:19:14 -0600

    Add server.js and index.js associations

--- models/index.js
+++ models/index.js
@@ -5,12 +5,21 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, { through: 'Product_Tags' });
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, { through: 'Tag_Products' });
 
 module.exports = {
   Product,
