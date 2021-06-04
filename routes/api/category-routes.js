d9293034a14612cc8a186f0a35baacfb6e499381
El Roble <san_lucas2005@yahoo.com>
2021-06-04 12:24:58 -0600

    Add POST method to category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -35,9 +35,17 @@ router.get('/:id', async (req, res) => {
   }  
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
-});
+    try {
+      const categoryData = await Category.create({
+        category_name: req.body.category_name,
+      });
+      res.status(200).json(categoryData);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
