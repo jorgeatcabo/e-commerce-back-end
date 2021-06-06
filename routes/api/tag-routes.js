e36d73056efbc814c0426b62963d4c65780f914e
El Roble <san_lucas2005@yahoo.com>
2021-06-06 16:28:53 -0600

    Add POST method for creating a Tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -36,8 +36,21 @@ router.get('/:id', async(req, res) => {
   }  
 });
 
-router.post('/', (req, res) => {
+router.post('/', async(req, res) => {
   // create a new tag
+ /* req.body should look like this...
+    {
+      tag_name: "yellow"
+    }
+  */
+    try {
+      const tagData = await Tag.create({
+        tag_name: req.body.tag_name,
+      });
+      res.status(200).json(tagData);
+    } catch (err) {
+      res.status(400).json(err);
+    }
 });
 
 router.put('/:id', (req, res) => {
