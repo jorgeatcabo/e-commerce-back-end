8e911b271201086cfd9f17d7b167e6d38cc2cb7c
El Roble <san_lucas2005@yahoo.com>
2021-06-06 15:45:57 -0600

    Add GET method for getting one product with its tags

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -23,7 +23,7 @@ router.get('/:id', async(req, res) => {
   // be sure to include its associated Category and Tag data
   try {
     const productData = await Product.findByPk(req.params.id, {
-      include: [{ model: Category },{ model: ProductTag }],
+      include: [{ model: Category},{ model: Tag, as: "tags"}],
     });
 
     if (!productData) {
